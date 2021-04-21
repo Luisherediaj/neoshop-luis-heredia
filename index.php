46102cff58aff38a92a63ae6d975663cf42c2a62
Luis <luis.here.j@gmail.com>
2021-04-21 19:44:53 +0200

    Slider dinámico, grid productos
    Modificación de js para que el slider sea dinámico.
    Añadido grid e imágenes de productos

--- index.php
+++ index.php
@@ -27,10 +27,10 @@ include "templates/header.php";
     <div class="slider">
         <!-- TODO: Desarrollar el marcado para el slider -->
         <ul class="slides">
-            <li><img src="./resources/slider/slide1.jpg" alt="slide1" /></li>
-            <li><img src="./resources/slider/slide2.jpg" alt="slide2" /></li>
-            <li><img src="./resources/slider/slide3.jpg" alt="slide3" /></li>
-            <li><img src="./resources/slider/slide4.jpg" alt="slide4" /></li>
+            <li class="mySlides"><img src="./resources/slider/slide1.jpg" alt="slide1" /></li>
+            <li class="mySlides"><img src="./resources/slider/slide2.jpg" alt="slide2" /></li>
+            <li class="mySlides"><img src="./resources/slider/slide3.jpg" alt="slide3" /></li>
+            <li class="mySlides"><img src="./resources/slider/slide4.jpg" alt="slide4" /></li>
         </ul>
     <div class="buttons">
         <div class="next">&#10095;</div>
@@ -39,11 +39,46 @@ include "templates/header.php";
 
     </div><!-- .slider -->
 
-
-
+    <h1>PRODUCTOS</h1>
     <div class="products">
         <!-- TODO: Desarrollar el marcado para los productos -->
+
+        <div class="item1">
+            <img alt="OLD SKOOL 36 DX" src="./resources/products/vans1.png">
+        </div>
+        <div class="item2">
+            <img alt="COMFYCUSH OLD SKOOL" src="./resources/products/vans2.png">
+        </div>
+        <div class="item3">
+            <img alt="OLD SKOOL DE ANTE Y LONA" src="./resources/products/vans3.png">
+        </div>
+        <div class="item4">
+            <img alt="OLD SKOOL DE ANTE SUAVE" src="./resources/products/vans4.png">
+        </div>
+        <div class="item5">
+            <img alt="cta1" src="./resources/cta/cta1.jpg">
+        </div>
+        <div class="item6">
+            <img alt="OLD SKOOL PRO" src="./resources/products/vans5.png">
+        </div>
+        <div class="item7">
+            <img alt="ANTI HERO OLD SKOOL PRO" src="./resources/products/vans6.png">
+        </div>
+        <div class="item8">
+            <img alt="ERA PRIMARY CHECK" src="./resources/products/vans7.png">
+        </div>
+        <div class="item9">
+            <img alt="cta2" src="./resources/cta/cta2.jpg">
+        </div>
+        <div class="item10">
+            <img alt="STYLE 36" src="./resources/products/vans8.png">
+        </div>
+
+
+
     </div><!-- .products -->
+    
+    
     <div class="load">
         <!-- TODO: BONUS. Desarrollar la funcionalidad para que el botón cargue más productos -->
         <span class="load__button button">Cargar más productos</span>
--- js/functions.js
+++ js/functions.js
@@ -1,57 +1,20 @@
 /**
  * TODO: Desarrollar el código del slider
  */
- window.addEventListener('load', () => {
- 
- 
-    // initial slide
-    let slide = 1;
- 
-    // total slides
-    let slides = document.querySelectorAll(".slider ul li");
-    total = slides.length;
- 
-    // show first side
-    showSlide(1);
- 
-    next = document.querySelector(".next");
-    prev = document.querySelector(".prev")
- 
-    /**
-     * event next button
-     */
-    next.addEventListener('click', (evt) => {
-        evt.preventDefault();
-        slide++;
-        if (slide > total) { slide = 1; }
-        showSlide(slide);
-    })
- 
-    /** 
-     * event prev button
-     */
-    prev.addEventListener("click", (evt) => {
-        evt.preventDefault();
-        slide--;
-        if (slide < 1) { slide = total; }
-        showSlide(slide);
-    })
- 
-    /**
-     * show slides
-     * 
-     * @param {number} n 
-     * @return {null}
-     * 
-     */
-    function showSlide(n) {
-        n--; // decrement 1
-        for (i = 0; i < slides.length; i++) {
-            (i == n) ? slides[n].style.display = "block" : slides[i].style.display = "none";
-        }
-    }
- 
-})
+ var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 
 
 /**
